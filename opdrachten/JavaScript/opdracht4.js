523fe566d7aa79e9cbb882760ac8dda61d4cb8fb
Michael Aular <michaelaular@gmail.com>
2021-11-25 16:54:55 +0100

    4.1 setup

---
+++ opdrachten/JavaScript/opdracht4.js
@@ -0,0 +1,12 @@
+function functieButton4() {
+    console.log("Hello World!")
+}
+
+const idVar = document.getElementById('naam_input')
+logName (idVar)
+
+
+function logName() {
+    console.log(logName)
+}
+
