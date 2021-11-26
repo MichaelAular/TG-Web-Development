bc7dc25be4922629223c8f0562b6fc66c32430d6
Michael Aular <michaelaular@gmail.com>
2021-11-26 17:02:13 +0100

    done for the weekend

--- opdrachten/JavaScript/opdracht4.js
+++ opdrachten/JavaScript/opdracht4.js
@@ -1,37 +1,38 @@
 function functieButton() {
-    console.log("Hello World!")
+  console.log("Hello World!");
 }
 
-let varInput = document.getElementById("input1")
+let varInput = document.getElementById("input1");
 
 function log1(varInput) {
-    console.log(varInput.value);
+  console.log(varInput.value);
 }
 
 function log2() {
-let inputNumber = document.getElementById("input2").value;
-let calculate = inputNumber => inputNumber * 2;
-let result = calculate(inputNumber);
-console.log("answer " + result);
+  let inputNumber = document.getElementById("input2").value;
+  let calculate = (inputNumber) => inputNumber * 2;
+  let result = calculate(inputNumber);
+  console.log("answer " + result);
 }
 
-// let antwoord42aI = document.getElementsByClassName("vraag42aI");
-// console.log(antwoord42aI);
-
-// let  antwoord42aII = document.getElementsByTagName("i");
-// console.log(antwoord42aII);
-
-
 function trigger42aI() {
-    let antwoord42aI = document.getElementsByClassName("vraag42aI");
-    antwoord42aI[0].style.color = "cyan";
+  let antwoord42aI = document.getElementsByClassName("vraag42aI");
+  antwoord42aI[0].style.color = "cyan";
 }
 
 function trigger42aII() {
-    let antwoord42aII = document.getElementsByTagName("span");
-    antwoord42aII[0].style.color = "cyan";
+  let antwoord42aII = document.getElementsByTagName("span");
+  antwoord42aII[1].style.color = "cyan";
 }
 
 function trigger42aIII() {
-    document.querySelector(".vraag42aIII").style.color = "cyan";
+  document.querySelector(".vraag42aIII").style.color = "cyan";
+}
+
+function trigger42aIV() {
+  document.querySelectorAll(".vraag42aIV")[1].style.color = "cyan";
+}
+
+function trigger42aV() {
+  document.getElementById("vraag42aV").innerHTML = "ook cyan!";
 }
