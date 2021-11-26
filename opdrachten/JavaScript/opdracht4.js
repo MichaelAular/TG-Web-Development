d97f283b1e0b1e0b20bf296a74f5c5e620f29332
Michael Aular <michaelaular@gmail.com>
2021-11-26 11:25:03 +0100

    4.1 done

--- opdrachten/JavaScript/opdracht4.js
+++ opdrachten/JavaScript/opdracht4.js
@@ -1,12 +1,18 @@
-function functieButton4() {
+function functieButton() {
     console.log("Hello World!")
 }
 
-const idVar = document.getElementById('naam_input')
-logName (idVar)
+let varInput = document.getElementById("input1")
 
 
-function logName() {
-    console.log(logName)
+function log1(varInput) {
+    console.log(varInput.value);
 }
 
+
+function log2() {
+let inputNumber = document.getElementById("input2").value;
+let calculate = inputNumber => inputNumber * 2;
+let result = calculate(inputNumber);
+console.log("answer " + result);
+}
