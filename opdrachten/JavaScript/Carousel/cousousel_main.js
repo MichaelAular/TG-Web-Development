2f14791ba805c9bddff48c151b7a0194d183a5a6
Michael Aular <michaelaular@gmail.com>
2021-12-07 20:54:21 +0100

    update carousel colors

--- opdrachten/JavaScript/Carousel/cousousel_main.js
+++ opdrachten/JavaScript/Carousel/cousousel_main.js
@@ -23,7 +23,18 @@ function showSlides(n) {
       dots[i].className = dots[i].className.replace(" active", "");
   }
 
+
+  if (n===0) {document.getElementById("setColor").style.color = "#7a0b0e"}
+  if (n===1) {document.getElementById("setColor").style.color = "#78b9cf"}
+  if (n===2) {document.getElementById("setColor").style.color = "#9a1c02"}
+  if (n===3) {document.getElementById("setColor").style.color = "#b93a60"}
+  if (n===4) {document.getElementById("setColor").style.color = "#0b2054"}
+  if (n===5) {document.getElementById("setColor").style.color = "#46c2da"}
+  if (n===6) {document.getElementById("setColor").style.color = "#7a0b0e"}
+  if (n===7) {document.getElementById("setColor").style.color = "#78b9cf"}
+
   slides[slideIndex-1].style.display = "block";
   dots[slideIndex-1].className += " active";
+
 }
 
