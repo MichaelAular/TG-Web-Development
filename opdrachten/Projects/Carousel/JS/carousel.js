80582105de37ad60f4137e6f9d7a68379e61675a
Michael Aular <michaelaular@gmail.com>
2021-12-08 17:04:46 +0100

    update

---
+++ opdrachten/Projects/Carousel/JS/carousel.js
@@ -0,0 +1,7 @@
+const track = document.querySelector('.carousel_track')
+const slides = Array.from(track.children)
+console.log(slides)
+
+//left slide left
+//reght slide right
+// click nav indicator move to slide
