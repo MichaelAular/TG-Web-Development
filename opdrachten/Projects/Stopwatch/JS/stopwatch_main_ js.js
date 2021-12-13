e9a5866741b6599652706d0bdcec8a2863249e2e
Michael Aular <michaelaular@gmail.com>
2021-12-13 16:44:48 +0100

    seiko watch update

--- opdrachten/Projects/Stopwatch/JS/stopwatch_main_ js.js
+++ opdrachten/Projects/Stopwatch/JS/stopwatch_main_ js.js
@@ -1,27 +1,40 @@
-/// /// /// /// construct  5 divs in stopwatch_container /// /// /// ///
-/// /// /// /// add class to div (logo, time, start, pause, stop) /// /// /// ///
-const logo = document.createElement('div');
-logo.classList.add('logo');
-container.appendChild(logo);
+/// /// /// /// construct  2 divs in stopwatch_container /// /// /// ///
+/// /// /// /// add class to div (logo, time) /// /// /// ///
+const logo = document.createElement('div')
+logo.classList.add('logo')
+container.appendChild(logo)
 
-const time = document.createElement('div');
-time.classList.add('time');
-container.appendChild(time);
+const time = document.createElement('div')
+time.classList.add('time')
+container.appendChild(time)
 
-const start = document.createElement('div');
-start.classList.add('start');
-container.appendChild(start);
+/// /// /// /// make 3 buttons (in start, pause, stop) in container /// /// /// ///
+const button_container = document.createElement('div')
+button_container.classList.add('button_container')
+container.appendChild(button_container)
 
-const pause = document.createElement('div');
-pause.classList.add('pause');
-container.appendChild(pause);
+const start_btn  = document.createElement('button')
+start_btn.classList.add('start', 'btn')
+start_btn.innerText = '▶'
+button_container.appendChild(start_btn)
 
-const stop = document.createElement('div');
-stop.classList.add('stop');
-container.appendChild(stop);
+const pause_btn  = document.createElement('button')
+pause_btn.classList.add('pause', 'btn')
+pause_btn.innerText = '┃┃'
+button_container.appendChild(pause_btn)
 
-/// /// /// /// make 3 buttons (in start, pause, stop) /// /// /// ///
+const stop_btn  = document.createElement('button')
+stop_btn.classList.add('stop', 'btn')
+stop_btn.innerText = '⬜'
+button_container.appendChild(stop_btn)
 
 
 /// /// /// /// add logo in logo /// /// /// ///
+const logo_letters = document.createElement('p')
+logo_letters.innerText = 'SEIKO'
+logo.appendChild(logo_letters)
+console.log(logo_letters)
+
 /// /// /// /// set time + funstions to buttons /// /// /// ///
+
+console.log(container)
