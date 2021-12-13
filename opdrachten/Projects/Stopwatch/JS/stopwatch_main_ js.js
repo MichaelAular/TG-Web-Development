85845884979dc514cb970c390a9c996f36cdf9e5
Michael Aular <michaelaular@gmail.com>
2021-12-13 11:01:38 +0100

    opzet stopwatch

---
+++ opdrachten/Projects/Stopwatch/JS/stopwatch_main_ js.js
@@ -0,0 +1,27 @@
+/// /// /// /// construct  5 divs in stopwatch_container /// /// /// ///
+/// /// /// /// add class to div (logo, time, start, pause, stop) /// /// /// ///
+const logo = document.createElement('div');
+logo.classList.add('logo');
+container.appendChild(logo);
+
+const time = document.createElement('div');
+time.classList.add('time');
+container.appendChild(time);
+
+const start = document.createElement('div');
+start.classList.add('start');
+container.appendChild(start);
+
+const pause = document.createElement('div');
+pause.classList.add('pause');
+container.appendChild(pause);
+
+const stop = document.createElement('div');
+stop.classList.add('stop');
+container.appendChild(stop);
+
+/// /// /// /// make 3 buttons (in start, pause, stop) /// /// /// ///
+
+
+/// /// /// /// add logo in logo /// /// /// ///
+/// /// /// /// set time + funstions to buttons /// /// /// ///
