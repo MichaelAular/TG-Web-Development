21320f51450cca22c25f0c87c53351d6a254ced3
Michael Aular <michaelaular@gmail.com>
2021-12-14 10:05:07 +0100

    time in watch

--- opdrachten/Projects/Stopwatch/JS/stopwatch_main_ js.js
+++ opdrachten/Projects/Stopwatch/JS/stopwatch_main_ js.js
@@ -1,5 +1,4 @@
-/// /// /// /// construct  2 divs in stopwatch_container /// /// /// ///
-/// /// /// /// add class to div (logo, time) /// /// /// ///
+/// /// /// /// construct  2 divs (logo, time) in stopwatch_container /// /// /// ///
 const logo = document.createElement('div')
 logo.classList.add('logo')
 container.appendChild(logo)
@@ -28,13 +27,41 @@ stop_btn.classList.add('stop', 'btn')
 stop_btn.innerText = '⬜'
 button_container.appendChild(stop_btn)
 
-
 /// /// /// /// add logo in logo /// /// /// ///
 const logo_letters = document.createElement('p')
 logo_letters.innerText = 'SEIKO'
 logo.appendChild(logo_letters)
-console.log(logo_letters)
+
+/// /// /// /// Create time /// /// /// ///
+function showTime() {
+    let date = new Date();
+    let displayTime = date.toLocaleTimeString();
+    time.innerText = displayTime;
+  }
+
+  setInterval(showTime, 1000);
 
 /// /// /// /// set time + funstions to buttons /// /// /// ///
 
+// let shownTime = document.createElement('p');
+
+// const minutes = document.createElement('p');
+// minutes.classList.add('minutes')
+// shownTime.appendChild(minutes)
+
+// const seconds = document.createElement('p');
+// seconds.classList.add('seconds')
+// shownTime.appendChild(seconds)
+
+// const centiSec = document.createElement('p');
+// centiSec.classList.add('centiSec')
+// shownTime.appendChild(centiSec)
+
+// shownTime = minutes + ' : ' + seconds + ' : ' + centiSec
+// time.appendChild(shownTime)
+// console.log(time)
+
+
+
+/// /// /// /// container log /// /// /// ///
 console.log(container)
