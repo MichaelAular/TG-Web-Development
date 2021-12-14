6a6a5e19b31f674302e3adf959748edcc16c61fa
Michael Aular <michaelaular@gmail.com>
2021-12-14 13:40:39 +0100

    stopwatch done

--- opdrachten/Projects/Stopwatch/JS/stopwatch_main_ js.js
+++ opdrachten/Projects/Stopwatch/JS/stopwatch_main_ js.js
@@ -7,6 +7,10 @@ const time = document.createElement('div')
 time.classList.add('time')
 container.appendChild(time)
 
+const stopWatch = document.createElement('div')
+stopWatch.classList.add('stopWatch')
+container.appendChild(stopWatch)
+
 /// /// /// /// make 3 buttons (in start, pause, stop) in container /// /// /// ///
 const button_container = document.createElement('div')
 button_container.classList.add('button_container')
@@ -15,16 +19,19 @@ container.appendChild(button_container)
 const start_btn  = document.createElement('button')
 start_btn.classList.add('start', 'btn')
 start_btn.innerText = '▶'
+start_btn.onclick = () => {startTimer()}
 button_container.appendChild(start_btn)
 
 const pause_btn  = document.createElement('button')
 pause_btn.classList.add('pause', 'btn')
 pause_btn.innerText = '┃┃'
+pause_btn.onclick = () => {pauseTimer()}
 button_container.appendChild(pause_btn)
 
 const stop_btn  = document.createElement('button')
 stop_btn.classList.add('stop', 'btn')
 stop_btn.innerText = '⬜'
+stop_btn.onclick = () => {stopTimer()}
 button_container.appendChild(stop_btn)
 
 /// /// /// /// add logo in logo /// /// /// ///
@@ -41,27 +48,75 @@ function showTime() {
 
   setInterval(showTime, 1000);
 
-/// /// /// /// set time + funstions to buttons /// /// /// ///
+// /// /// /// /// set time + funstions to buttons /// /// /// ///
+const timer = document.createElement('div');
+stopWatch.appendChild(timer)
+timer.innerText = '00:00:00'
 
-// let shownTime = document.createElement('p');
+let min = 0;
+let sec = 0;
+let centi = 0;
+let stoptime = true;
 
-// const minutes = document.createElement('p');
-// minutes.classList.add('minutes')
-// shownTime.appendChild(minutes)
+function startTimer() {
+  if (stoptime == true) {
+        stoptime = false;
+        stopWatch.style.opacity = '1'
+        timerCycle();
+    }
+}
+function pauseTimer() {
+  if (stoptime == false) {
+    stoptime = true;
+  }
+}
 
-// const seconds = document.createElement('p');
-// seconds.classList.add('seconds')
-// shownTime.appendChild(seconds)
+function timerCycle() {
+    if (stoptime == false) {
+    centi = parseInt(centi);
+    sec = parseInt(sec);
+    min = parseInt(min);
 
-// const centiSec = document.createElement('p');
-// centiSec.classList.add('centiSec')
-// shownTime.appendChild(centiSec)
+    centi = centi + 1;
 
-// shownTime = minutes + ' : ' + seconds + ' : ' + centiSec
-// time.appendChild(shownTime)
-// console.log(time)
+    if (centi == 100) {
+      sec = sec + 1;
+      centi = 0;
+    }
+    if (sec == 60) {
+      min = min + 1;
+      sec = 0;
+      centi = 0;
+    }
 
+    if (centi < 10) {
+      centi = '0' + centi;
+    }
+    if (sec < 10) {
+      sec = '0' + sec;
+    }
+    if (min	 < 10) {
+      min = '0' + min;
+    }
 
+    timer.innerHTML = min + ':' + sec + ':' + centi;
 
-/// /// /// /// container log /// /// /// ///
-console.log(container)
+    setTimeout("timerCycle()", 10);
+  }
+}
+
+function resetTimer() {
+    timer.innerHTML = '00:00:00';
+    stoptime = true;
+    hr = 0;
+    sec = 0;
+    min = 0;
+}
+
+function stopTimer() {
+  stopWatch.style.opacity = '0'
+  stoptime = true;
+    hr = 0;
+    sec = 0;
+    min = 0;
+}
