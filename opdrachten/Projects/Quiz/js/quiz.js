ab1eb1bd52d06e58b65ed6a34c8f47ac0222f285
Michael Aular <michaelaular@gmail.com>
2021-12-16 16:52:45 +0100

    opzet quiz

---
+++ opdrachten/Projects/Quiz/js/quiz.js
@@ -0,0 +1,11 @@
+/// /// create buttons /// ///
+const start_btn  = document.createElement('button')
+start_btn.classList.add('start_btn', 'btn')
+start_btn.innerText = 'START!'
+// start_btn.onclick = () => {stopTimer()}
+container.appendChild(start_btn)
+
+
+/// /// create question field /// ///
+/// /// create question /// ///
+/// /// create answers /// ///
