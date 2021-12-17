cb35d774617b3396bd326515b836416d75b4561c
Michael Aular <michaelaular@gmail.com>
2021-12-18 00:16:46 +0100

    serveral qustion posibility

--- opdrachten/Projects/Quiz/js/quiz.js
+++ opdrachten/Projects/Quiz/js/quiz.js
@@ -2,7 +2,18 @@
 /// /// create start/// ///
 const page_start = document.querySelector('.page_Start');
 const container = document.querySelector('.container');
-const start_btn  = document.createElement('button')
+const start_btn  = document.createElement('button');
+let pageCounter = 0
+
+/// /// create questions /// ///
+const questionArray = ["49 - 32", "70 - 14", "80 - 15", "10 - 20", "56 + 11", "21 - 16"];
+
+/// /// create answers /// ///
+const answersArray = [
+"13", "17", "-17", "16", "697",
+"1", "2", "3", "4", "5",
+"a", "b", "c", "d", "e"]
+
 start_btn.classList.add('start_btn')
 start_btn.innerText = 'START!'
 page_start.appendChild(start_btn)
@@ -12,7 +23,7 @@ start_btn.onclick = () => {clickStart()}
 function clickStart() {
     container.classList.remove('page_Start');
     container.removeChild(start_btn);
-    container.classList.add('page_Question', 'page1');
+    container.classList.add('page_Question');
     constructButtons()
     constructAnswerblocks()
     constructQuestion()
@@ -24,13 +35,13 @@ function constructButtons() {
     prev_btn.classList.add('prev_btn', 'btn');
     prev_btn.innerText = 'vorrige';
     container.appendChild(prev_btn);
-    // prev_btn.onclick = () => {clickPrev()}
+    prev_btn.onclick = () => {clickPrev()}
 
     const next_btn  = document.createElement('button');
     next_btn.classList.add('next_btn', 'btn');
     next_btn.innerText = 'volgende';
     container.appendChild(next_btn);
-    // prev_btn.onclick = () => {clickNext()}
+    next_btn.onclick = () => {clickNext()}
 }
 
 function constructAnswerblocks() {
@@ -47,12 +58,18 @@ function constructAnswerblocks() {
         numberCircle.classList.add('number'+[i], 'number')
         numberCircle.innerText = i
         option.appendChild(numberCircle)
+
+        const answerText = document.createElement('div')
+        answerText.classList.add('answerText'+[i], 'answerText')
+        answerText.innerText = answersArray[(pageCounter*5-1)+i]
+        option.appendChild(answerText)
     }
 }
 
 function constructQuestion() {
     const question = document.createElement('div')
     question.classList.add('question')
+    question.innerText = 'What is ' + questionArray[pageCounter]
     container.appendChild(question)
 }
 
@@ -68,8 +85,29 @@ function constructTitle() {
 
     const pageIndex= document.createElement('div')
     pageIndex.classList.add('pageIndex')
-    pageIndex.innerText = "1 / 6"
+    pageIndex.innerText = pageCounter+1 + " / " + questionArray.length
     titleContainer.appendChild(pageIndex)
 }
 
-/// /// create answers /// ///
+function clickNext() {
+    pageCounter++
+    if (pageCounter > questionArray.length-1) {
+        pageCounter = questionArray.length-1
+    }
+    constructTitle()
+    constructAnswerblocks()
+    constructQuestion()
+    console.log(pageCounter)
+}
+
+function clickPrev() {
+    pageCounter--
+    if (pageCounter < 0 ) {
+        pageCounter = 0
+    }
+    constructTitle()
+    constructAnswerblocks()
+    constructQuestion()
+
+    console.log(pageCounter)
+}
