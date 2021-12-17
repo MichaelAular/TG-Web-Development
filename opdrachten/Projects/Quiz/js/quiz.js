604001ab2ce603266235ba14704a5ac3e2ef5d9e
Michael Aular <michaelaular@gmail.com>
2021-12-17 15:13:51 +0100

    quiz update 3

--- opdrachten/Projects/Quiz/js/quiz.js
+++ opdrachten/Projects/Quiz/js/quiz.js
@@ -1,23 +1,23 @@
-/// /// create pages /// ///
+
+/// /// create start/// ///
 const page_start = document.querySelector('.page_Start');
 const container = document.querySelector('.container');
-
-/// /// create buttons /// ///
 const start_btn  = document.createElement('button')
 start_btn.classList.add('start_btn')
 start_btn.innerText = 'START!'
 page_start.appendChild(start_btn)
 start_btn.onclick = () => {clickStart()}
 
-/// /// create question field /// ///
-/// /// create question /// ///
-/// /// create answers /// ///
-
 /// /// /// functions /// /// ///
 function clickStart() {
-    container.classList.remove('page_Start');
-    container.removeChild(start_btn);
-    container.classList.add('page_Question', 'page1');
+        container.classList.remove('page_Start');
+        container.removeChild(start_btn);
+        container.classList.add('page_Question', 'page1');
+        constructButtons()
+        constructAnswerblocks()
+    }
+
+function constructButtons() {
     const prev_btn  = document.createElement('button');
     prev_btn.classList.add('prev_btn', 'btn');
     prev_btn.innerText = 'BACK';
@@ -29,3 +29,20 @@ function clickStart() {
     container.appendChild(next_btn);
     // prev_btn.onclick = () => {clickNext()}
 }
+
+function constructAnswerblocks() {
+    for (i=1 ; i < 6 ; i++) {
+        const option = document.createElement('div')
+        option.classList.add('option'+[i], 'option')
+        container.appendChild(option);
+
+        const number = document.createElement('div')
+        option.classList.add('number'+[i], 'number')
+        number.innerText = i
+        option.appendChild(number)
+    }
+}
+
+/// /// create question field /// ///
+/// /// create question /// ///
+/// /// create answers /// ///
